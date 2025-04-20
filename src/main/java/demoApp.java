51d58b2b4498bb8cb948f880f4ffcce77e5b8d0d
S Dineshkumar <107368289+DineshKumar38@users.noreply.github.com>
2025-04-21 01:24:05 +0530

    Update demoApp.java

--- src/main/java/demoApp.java
+++ src/main/java/demoApp.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class demoApp {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
